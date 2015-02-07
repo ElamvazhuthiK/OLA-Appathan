dce89e09f9b4e74311aafc6c45a7292eda31f40d
elamvazhuthi.k <elamvazhuthi@marketsimplified.com>
2015-02-07 18:04:46 +0530

    add git ignore file

---
+++ AppathonTemplate/SignUp/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  AppathonTemplate
+//
+//  Created by inxs tech on 07/02/15.
+//  Copyright (c) 2015 Market Simplified. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SignUpViewController : BaseViewController
+
+@end
---
+++ AppathonTemplate/SignUp/SignView.h
@@ -0,0 +1,13 @@
+//
+//  SignView.h
+//  AppathonTemplate
+//
+//  Created by inxs tech on 07/02/15.
+//  Copyright (c) 2015 Market Simplified. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@interface SignView : BaseView
+
+@end
